49b8c5864bcb181e3c6a711fb1290dab0a847955
Nathan <npbehunin@gmail.com>
2018-11-26 16:45:52 -0700

    Began fixing enemy movement
    Edited script so enemy moves after every set amount of seconds. Still buggy. Enemy is also being worked on to have his own box collider hit the player.

---
+++ DGM_2211_Project/Assets/Macros/BreakCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakCollision : MonoBehaviour
+{
+	public BreakableObject breakobject1;
+	public BreakableObject breakobject2;
+	public BreakableObject breakobject3;
+	public BreakableObject breakobject4;
+	public BreakableObject breakobject5;
+	public BreakableObject breakobject6;
+	public BreakableObject breakobject7;
+	public BreakableObject breakobject8;
+	public BreakableObject breakobject9;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Bullet"))
+		{
+			Debug.Log("Bullet collision!");
+			breakobject1.EnableKinematic();
+			breakobject2.EnableKinematic();
+			breakobject3.EnableKinematic();
+			breakobject4.EnableKinematic();
+			breakobject5.EnableKinematic();
+			breakobject6.EnableKinematic();
+			breakobject7.EnableKinematic();
+			breakobject8.EnableKinematic();
+			breakobject9.EnableKinematic();
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ DGM_2211_Project/Assets/Macros/BreakableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+	public Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.detectCollisions = false;
+		rb.useGravity = false;
+	}
+	
+	public void EnableKinematic() {
+		rb.detectCollisions = true;
+		rb.useGravity = true;
+	}
+}
